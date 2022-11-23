a0b2e5cf50f2d5b382ae33cd88c5dc1016ce7c60
Perin-Mangukiya <mangukiyaperin3016@gmail.com>
2022-11-23 18:17:32 +0530

    App ready for production

---
+++ build/precache-manifest.e37484058627149ce8a3fdb09dc47cde.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cd48fa498110b64c7615a02bfb4e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "76f376d9e16aa70c5f1a",
+    "url": "/static/js/main.7d7c1cb1.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
